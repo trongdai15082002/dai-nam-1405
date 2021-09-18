0232523234c9a59ca73b59275ac075d9d2aed4cf
trongdai15082002 <89824288+trongdai15082002@users.noreply.github.com>
2021-09-18 15:55:57 +0700

    Add files via upload

--- lists.py
+++ lists.py
@@ -1,4 +1,10 @@
-k = 15
-h = 8
-s = k + h
-print(s)
+list = [2,6,3,8,1,4,7]
+a = len(list)
+
+for i in range(0 ,a -1):
+    for j in range(i+1,a):
+        if(list[i]>list[j]):
+            b = list[i]
+            list[i] = list[j]
+            list[j] = b
+            print(list)
