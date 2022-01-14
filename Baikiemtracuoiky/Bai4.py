1cc4b5c87f3d3ee71100ef5410c53ae980d2bafd
trongdai15082002 <89824288+trongdai15082002@users.noreply.github.com>
2022-01-14 09:49:35 +0700

    Delete Bai4.py

--- Baikiemtracuoiky/Bai4.py
+++
@@ -1,8 +0,0 @@
-nameid = input ( "Nhập tên và id chuỗi:" )
-id = []
-id = re . sub ( r '\ D' , '' , nameid )
-l = lại . findall ( r '\ d' , id )
-t = tuple ( l )
-print ( "Mã sinh viên là:" , id )
-in ( l )
-in ( t )
